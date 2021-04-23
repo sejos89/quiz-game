1ca16cd927e98d9bfd03362ea81a430a1c210f8c
Jose López <sejosin89@gmail.com>
2021-04-23 18:27:01 +0200

    new changes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -53,58 +53,5 @@ export default function Navigation() {
         </p>
       </div>
     </>
-    // <div
-    //   className="header"
-    //   style={toggleTheme ? { backgroundColor: "#f3e3e1" } : {}}
-    // >
-    //   <div
-    //     className="navbar"
-    //     style={toggleTheme ? { backgroundColor: "#f3e3e1" } : {}}
-    //   >
-    //     <img
-    //       src={`${process.env.PUBLIC_URL}/assets/ugr-horizontal-color.svg`}
-    //     />
-    //     <ul>
-    //       <li>
-    //         <ToggleButton />
-    //       </li>
-    //       <li>
-    //         <Link
-    //           to="/team"
-    //           className={
-    //             location.pathname === "/team"
-    //               ? "chosen-topic"
-    //               : toggleTheme
-    //               ? "dark-button"
-    //               : ""
-    //           }
-    //         >
-    //           Team
-    //         </Link>
-    //       </li>
-    //       <li>
-    //         <Link
-    //           to="/test"
-    //           className={
-    //             location.pathname === "/test"
-    //               ? "chosen-topic"
-    //               : toggleTheme
-    //               ? "dark-button"
-    //               : ""
-    //           }
-    //         >
-    //           Tests
-    //         </Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-    //   <div className="subheader">
-    //     <p style={toggleTheme ? { color: "black" } : {}}>
-    //       HERRAMIENTAS VIRTUALES EN INGLÉS PARA UN APRENDIZAJE INTERACTIVO Y
-    //       DIVERTIDO DE LA QUÍMICA: CHEMGAME. Proyecto de Innovación Docente
-    //       (20-07). Convocatoria PIBD 2020-2022
-    //     </p>
-    //   </div>
-    // </div>
   );
 }
